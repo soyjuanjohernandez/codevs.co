20db83c4283265d76c6f40e5543805db630fa4a0
soyJuanjoHernandez <juanhernandez.code@gmail.com>
2024-11-15 16:07:41 -0500

    Add favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'CODEVS.co',
   tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon1.ico',
 
   // Set the production url of your site here
   url: 'https://your-docusaurus-site.example.com',
