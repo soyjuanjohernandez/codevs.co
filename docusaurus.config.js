7cecdec86e082e5547ad22c27825247b15294ab7
cequintero <ce.quintero@uniandes.edu.co>
2024-12-14 22:05:28 -0500

    configurar modo oscuro #2 y actualizacion de post de bienvenida, se agregan tags y se eliminan post si contenido autogenerados

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,8 +32,8 @@ const config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'es',
+    locales: ['es'],
   },
 
   presets: [
@@ -77,7 +77,7 @@ const config = {
       // image: '/static/main_500px.png',
       colorMode: {
         defaultMode: 'light',
-        disableSwitch: true,
+        disableSwitch: false,
         respectPrefersColorScheme: false,
       },
       navbar: {
