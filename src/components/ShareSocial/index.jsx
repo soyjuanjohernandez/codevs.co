4d877f1330942985ab06a19995554383f72d6829
soyJuanjoHernandez <juanhernandez.code@gmail.com>
2025-01-17 11:34:16 -0500

    fix access to window object in to ShareButtons component

--- src/components/ShareSocial/index.jsx
+++ src/components/ShareSocial/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
   FacebookShare,
   LinkedinShare,
@@ -10,8 +11,18 @@ import {
 import "./styles.css";
 
 export default function ShareButtons() {
-  const url = "window.location.href";
-  const documentTiltle = "document.title";
+  const [info, setInfo] = useState({
+    url: "",
+    title: "",
+  });
+
+  useEffect(() => {
+    const url = window.location.href;
+    const title = document.title;
+    setInfo({ url, title });
+
+  }, []);
+
   const borderRadius = 8;
   const size = 48;
 
@@ -21,39 +32,40 @@ export default function ShareButtons() {
       <h3>Compartir</h3>
       <br></br>
       <FacebookShare
-        url={url}
-        quote={documentTiltle}
+        url={info.url}
+        quote={info.title}
         borderRadius={borderRadius}
         size={size}
         className="cosa"
       />
 
       <TwitterShare
-        url={url}
-        title={documentTiltle}
+        url={info.url}
+        title={info.title}
         borderRadius={borderRadius}
         size={size}
       />
 
       <LinkedinShare
-        url={url}
+        url={info.url}
         blankTarget={true}
         borderRadius={borderRadius}
         size={size}
       />
 
       <WhatsappShare
-        url={url}
-        title={documentTiltle}
+        url={info.url}
+        title={info.title}
         separator=":: "
         blankTarget={true}
         borderRadius={borderRadius}
         size={size}
       />
 
-      <TelegramShare url={url} borderRadius={borderRadius} size={size} />
+      <TelegramShare url={info.url} borderRadius={borderRadius} size={size} />
 
-      <RedditShare url={url} borderRadius={borderRadius} size={size} />
+      <RedditShare url={info.url} borderRadius={borderRadius} size={size} />
+
     </div>
   );
 }
