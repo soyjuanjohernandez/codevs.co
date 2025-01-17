1db87bf59592be6398ff5ac4236c14cda94fcbca
soyJuanjoHernandez <juanhernandez.code@gmail.com>
2025-01-17 15:51:20 -0500

    Fix title an url in share buttons

--- src/components/ShareSocial/index.jsx
+++ src/components/ShareSocial/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import {
   FacebookShare,
   LinkedinShare,
@@ -10,61 +10,53 @@ import {
 
 import "./styles.css";
 
-export default function ShareButtons() {
-  const [info, setInfo] = useState({
-    url: "",
-    title: "",
-  });
-
-  useEffect(() => {
-    const url = window.location.href;
-    const title = document.title;
-    setInfo({ url, title });
-
-  }, []);
+export default function ShareButtons({url, title}) {
 
   const borderRadius = 8;
   const size = 48;
 
   return (
     <div className="share-buttons">
+
+      <h1>{title}</h1>
+
       <br></br>
       <h3>Compartir</h3>
       <br></br>
       <FacebookShare
-        url={info.url}
-        quote={info.title}
+        url={url}
+        quote={title}
         borderRadius={borderRadius}
         size={size}
         className="cosa"
       />
 
       <TwitterShare
-        url={info.url}
-        title={info.title}
+        url={url}
+        title={title}
         borderRadius={borderRadius}
         size={size}
       />
 
       <LinkedinShare
-        url={info.url}
+        url={url}
         blankTarget={true}
         borderRadius={borderRadius}
         size={size}
       />
 
       <WhatsappShare
-        url={info.url}
-        title={info.title}
+        url={url}
+        title={title}
         separator=":: "
         blankTarget={true}
         borderRadius={borderRadius}
         size={size}
       />
 
-      <TelegramShare url={info.url} borderRadius={borderRadius} size={size} />
+      <TelegramShare url={url} borderRadius={borderRadius} size={size} />
 
-      <RedditShare url={info.url} borderRadius={borderRadius} size={size} />
+      <RedditShare url={url} borderRadius={borderRadius} size={size} />
 
     </div>
   );
